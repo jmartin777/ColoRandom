215cfc64c354aba780acac26c0c7797e535e5b6e
Alexys Jordan <alexyskjordan@gmail.com>
2023-01-07 18:25:11 -0700

    Feat: lock colors

--- scripts.js
+++ scripts.js
@@ -16,16 +16,12 @@ var color5 = new Color()
 var allColors = new Palette(newRandomColor());
 var rightSection = document. querySelector('.right-section')
 var savedPalettesContainer = document.querySelector('#savedBox1')
-var locked1 = document.querySelector('#lock1')
-var locked2 = document.querySelector('#lock2')
-var locked3 = document.querySelector('#lock3')
-var locked4 = document.querySelector('#lock4')
-var locked5 = document.querySelector('#lock5')
-var unlocked1 = document.querySelector('#unlock1')
-var unlocked2 = document.querySelector('#unlock2')
-var unlocked3 = document.querySelector('#unlock3')
-var unlocked4 = document.querySelector('#unlock4')
-var unlocked5 = document.querySelector('#unlock5')
+
+var box1 = document.getElementById('box1')
+var box2 = document.getElementById('box2')
+var box3 = document.getElementById('box3')
+var box4 = document.getElementById('box4')
+var box5 = document.getElementById('box5')
 var boxes = document.querySelectorAll('.box')
 var colorBoxContainer = document.querySelector('.color-boxes-container')
 var codes = document.querySelectorAll('.color-hex')
@@ -35,7 +31,12 @@ var buttonSavePalette = document.getElementById('save');
 
 window.addEventListener('load', updatePalette);
 buttonSavePalette.addEventListener('click', saveButtonClick)
-buttonNewPalette.addEventListener('click', updatePalette);
+buttonNewPalette.addEventListener('click', checkLock);
+box1.addEventListener('click', lockToggle)
+box2.addEventListener('click', lockToggle)
+box3.addEventListener('click', lockToggle)
+box4.addEventListener('click', lockToggle)
+box5.addEventListener('click', lockToggle)
 
 function updatePalette() {
   allColors = new Palette(newRandomColor()) ;
@@ -50,26 +51,40 @@ function saveButtonClick() {
 }
 
 function newRandomColor() {
-  var firstColor = color1.getRandomColor()
-  var secondColor = color2.getRandomColor()
-  var thirdColor = color3.getRandomColor()
-  var fourthColor = color4.getRandomColor()
-  var fifthColor = color5.getRandomColor()
+  var color1 = new Color()
+  var color2 = new Color()
+  var color3 = new Color()
+  var color4 = new Color()
+  var color5 = new Color()
+  // var firstColor = color1.getRandomColor()
+  // var secondColor = color2.getRandomColor()
+  // var thirdColor = color3.getRandomColor()
+  // var fourthColor = color4.getRandomColor()
+  // var fifthColor = color5.getRandomColor()
+  
+    color1.getRandomColor()
+    color2.getRandomColor()
+    color3.getRandomColor()
+    color4.getRandomColor()
+    color5.getRandomColor()
+  
 
-var NewPaletteOnClick = [firstColor, secondColor, thirdColor, fourthColor, fifthColor];
+
+var NewPaletteOnClick = [color1, color2, color3, color4, color5];
 
 return NewPaletteOnClick;
 }
+
 
 function setRandomColor() {
   
   for (let i = 0; i < 5; i++) {
     boxes[i].innerHTML = `
-    <div class="box-details" style="background-color:${allColors.colors[i]}">
-    <p class="color-hex" id="code1">${allColors.colors[i]}</p>
+    <div class="box-details" id="box${i+1}" style="background-color:${allColors.colors[i].color}">
+    <p class="color-hex" id="code${i}">${allColors.colors[i].color}</p>
       <span class="material-symbols-outlined">
-      <button class="unlock-emoji"id="unlock1">🔓</button>
-      <button class="lock-emoji hidden" id="lock1">🔐</button>
+      <button class="unlock-emoji"id="unlock${i+1}">🔓</button>
+      <button class="lock-emoji hidden" id="lock${i+1}">🔐</button>
     </span>
   </div>`
   }
@@ -90,24 +105,55 @@ function renderRightSection() {
     <div class="mini-box-details" style="background-color:${savedPalettes[i].colors[2]}"></div>   
     <div class="mini-box-details" style="background-color:${savedPalettes[i].colors[3]}"></div>
     <div class="mini-box-details" style="background-color:${savedPalettes[i].colors[4]}"></div>
-  </section>
- `
-
+    <button class="trash-button" id="trash">🚫</button>
+    </section>
+    `
+    
   }
 
 }
 
+function lockToggle (event) {
+  console.log(event.target)
+    for (var i = 1; i < 6; i++ ) {
+    if (event.target.id === `box${i}`) {
+      var lock = document.querySelector(`#lock${i}`)
+      var unlock = document.querySelector(`#unlock${i}`)
+    allColors.toggleLock(i-1)
+    lock.classList.toggle('hidden')
+    unlock.classList.toggle('hidden')
+    }
+  }
+}
 
+function checkLock(){
+if(allColors.colors[0].locked === false) { 
+  allColors.colors[0].getRandomColor()
+}
+if(allColors.colors[1].locked === false) { 
+  allColors.colors[1].getRandomColor()
+}
+if(allColors.colors[2].locked === false) { 
+  allColors.colors[2].getRandomColor()
+}
+if(allColors.colors[3].locked === false) { 
+  allColors.colors[3].getRandomColor()
+}
+if(allColors.colors[4].locked === false) { 
+  allColors.colors[4].getRandomColor()
 
+}
+setRandomColor()
+updateLock()
 
-//    function lockColor() {
-   
-//     unlocked1.classList.add('hidden')
-//     locked1.classList.remove('hidden');
-//   };
-
-// function unlockColor(event) {
-//   unlocked1.classList.remove('hidden')
-//   locked1.classList.add('hidden')
-// }
-
+}
+function updateLock() {
+  for(var i=0; i < allColors.colors.length; i++) {
+    if(allColors.colors[i].locked) {
+      var lock = document.querySelector(`#lock${i+1}`)
+      var unlock = document.querySelector(`#unlock${i+1}`)
+      unlock.classList.add('hidden')
+      lock.classList.remove('hidden')
+    }
+  }
+}
